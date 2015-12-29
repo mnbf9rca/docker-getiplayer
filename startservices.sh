09fe3df06bb5ef6133e0c0afa802db1e0510262b
mnbf9rca <robert.aleck@cynexia.com>
2015-12-29 23:35:44 +0000

    Update startservices.sh

--- startservices.sh
+++ startservices.sh
@@ -1,3 +1,15 @@
+  #Get docker env timezone and set system timezone
+  echo "setting the correct local time"
+  echo $TZ > /etc/timezone
+  export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
+  dpkg-reconfigure tzdata
+  
+  #fix memory issue
+  echo "increasing shared memory"
+  umount /dev/shm
+  mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=${MEM:-4096M} tmpfs /dev/shm
+  
+  echo "starting services"
   service mysql start
   service apache2 start
   service zoneminder start
