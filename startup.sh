72379b498d675ee8b393fa885b1724acd789b620
mnbf9rca <robert.aleck@cynexia.com>
2015-12-29 23:55:56 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,22 @@
+  # check if there is a /config/zm folder. 
+  # - If there is, and if it has content, assume the DB is in it and so update the DB.
+  # - if there isn't, create it
+  
+  if [ "$(ls -A /config/zm)" ]; then
+    echo "/config/zm not empty; creating symlink and updating schema"
+    # rm /var/lib/mysql/zm # <-- not sure if we need this?
+    ln -s /config/zm/ /var/lib/mysql/zm/
+    chown -R mysql:mysql /var/lib/mysql
+    /usr/bin/zmupdate.pl
+  else
+    echo "/config/zm not found or empty; creating symlink and creating DB"
+    mkdir --parents /config/zm
+    chmod -R go+rw /config
+    ln -s /config/zm/ /var/lib/mysql/zm/
+    chown -R mysql:mysql /var/lib/mysql    
+    mysql < /usr/share/zoneminder/db/zm_create.sql
+  fi
+  
   #Get docker env timezone and set system timezone
   echo "setting the correct local time"
   echo $TZ > /etc/timezone
